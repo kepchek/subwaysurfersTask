9a86c7aabfc00ec0dcb25240fc6fe92d17b82cc3
Kepchek <112941559+kepchek@users.noreply.github.com>
2024-07-02 22:21:01 +0300

    Almost done spawn and move level sections
    НУЖНО УБРАТЬ КОСТЫЛЬ!!!

--- PracticeTask1(SubwaySurfers/Assets/Scripts/LevelManager.cs
+++ PracticeTask1(SubwaySurfers/Assets/Scripts/LevelManager.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject levelSectionPrefab; 
+    [SerializeField] private int initialSections = 6; //Кол-во секций вначале  <-- Можно трогать
+    [SerializeField] private float sectionLength = 20.0f;   //Не трогать
+    [SerializeField] private float levelSpeed = 5.0f; //Тоже можно трогать
+    [SerializeField] private Transform player;  //А тут нехуй трогать
+
+    private Queue<GameObject> sectionsQueue = new Queue<GameObject>(); //Очередь секций уровня
+    private float spawnZ = 0.0f; //Позиция по оси Z для появления новых секций
+
+    private void Start() 
     {
-        
+        for(int i = 0; i < initialSections; i++)
+        {
+            SpawnSection();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        MoveSections();
+
+        if(sectionsQueue.Peek().transform.position.z < player.position.z - sectionLength)
+        {
+            RemoveSection();
+            FixSpawnSection();//Почему здесь фиксспавнсекшн? Потому что я дебил, а ещё есть баг, в том что метод не универсальный и я спёкся его чинить и придумал крутой костыль! Завтра попробую починить, удачи!
+        }
     }
+
+    void SpawnSection()
+    {
+        GameObject newSection = Instantiate(levelSectionPrefab, new Vector3(0, 0, spawnZ), Quaternion.identity);
+        sectionsQueue.Enqueue(newSection);
+        spawnZ += sectionLength;
+    }
+
+    void FixSpawnSection()
+    {
+        GameObject newSection = Instantiate(levelSectionPrefab, new Vector3(0, 0, spawnZ-20), Quaternion.identity);
+        sectionsQueue.Enqueue(newSection);
+    }
+
+    void MoveSections()
+    {
+        foreach(var section in sectionsQueue)
+        {
+            section.transform.position -= new Vector3(0, 0, levelSpeed * Time.deltaTime);
+        }
+    }
+
+    void RemoveSection()
+    {
+        GameObject oldSection = sectionsQueue.Dequeue();
+        Destroy(oldSection);
+    }
+
+    
+
+
 }
