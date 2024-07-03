94fd7a654b7058dc84bfb02570df0d380822d8a9
Skrat <146776521+skrat1k@users.noreply.github.com>
2024-07-03 15:01:49 +0300

    add pool

---
+++ PracticeTask1(SubwaySurfers/Assets/Scripts/Pool.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> where T : MonoBehaviour
+{
+    public T prefab { get; }    
+    
+    public Transform container {get;}
+
+    private List<T> poolList;
+
+    public Pool(T prefab, int count, Transform container)
+    {
+        this.prefab = prefab;
+        this.container = container;
+        this.CreatePool(count);
+    }
+
+    private void CreatePool(int count)
+    {
+        this.poolList = new List<T>();
+
+        for (int i = 0; i < count; i++)
+        {
+            CreateObj();
+        }
+    }
+
+    private T CreateObj()
+    {
+        var createdObj = Object.Instantiate(this.prefab, this.container);
+        createdObj.gameObject.SetActive(false);
+        this.poolList.Add(createdObj);
+        return createdObj;
+    }
+
+    public bool HasFreeElement(out T element)
+    {
+        foreach ( var mono in poolList)
+        {
+            if(!mono.gameObject.activeInHierarchy)
+            {
+                element = mono;
+                mono.gameObject.SetActive(true);
+                return true;
+            }
+        }
+        
+        element = null;
+        return false;
+    }
+
+    public T GetFreeElement()
+    {
+        if(HasFreeElement(out var element))
+        {
+            return element;
+        }
+
+        throw new System.Exception($"Дурила, нет у тебя свободных элементов");
+
+    }
+}
